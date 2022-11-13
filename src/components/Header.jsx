e66a0400697d49946e8a0f8087a448f74719530a
mulleragustin <agustinmuller98@gmail.com>
2022-11-13 12:56:08 -0300

    hola

---
+++ src/components/Header.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+
+import Navbar from "./Navbar";
+
+const Header = () => {
+    return (
+        <div className="container">
+            <div className="row ">
+                <div >
+                    <Navbar />
+                </div>
+               
+            </div>
+        </div>
+    )
+}
+
+export default Header;
