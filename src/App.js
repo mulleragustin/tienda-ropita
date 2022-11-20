f4afafe21f257345f69bc2915377877adfc8f40a
mulleragustin <agustinmuller98@gmail.com>
2022-11-20 19:25:30 -0300

    asd

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 
 import './App.css';
+import Carousel from './components/Carousel';
 import Header from './components/Header';
 
 function App() {
   return (
     <div>
       <Header/>
+      <Carousel/>
     </div>
   );
 }
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,124 +1,32 @@
-import React, { useState } from 'react'
-import styled from 'styled-components'
-import BurguerButton from './BurguerButton'
+import React from 'react'
+
 
 function Navbar() {
 
-  const [clicked, setClicked] = useState(false)
-  const handleClick = () => {
-    //cuando esta true lo pasa a false y vice versa
-    setClicked(!clicked)
-  }
+  
   return (
-    <>
-      <NavContainer className='justify-content-space-between container-fluid'>
+    
+      <div className='justify-content-space-between'>
         <h2>Ropita</h2>
-        <div className={`links ${clicked ? 'active' : ''} `}>
-          <a onClick={handleClick} href="#h">Home</a>
-          <a onClick={handleClick} href="#h">Tienda</a>
-          <a onClick={handleClick} href="#h">Marcas</a>
+        <div >
+          <a  href="#h">Home</a>
+          <a  href="#h">Tienda</a>
+          <a  href="#h">Marcas</a>
 
         </div>
-        <div /* className={`links ${clicked ? 'active' : ''} `} */>
-          <a /* onClick={handleClick} */ href="#h"><img src="icons/user.svg" alt="user"/></a>
-          <a /* onClick={handleClick} */ href="#h"><img src="icons/star.svg" alt="fav"/></a>
-          <a /* onClick={handleClick} */ href="#h"><img src="icons/shopping-cart.svg" alt="cart"/></a>
+        <div >
+          <a href="#h"><img src="icons/user.svg" alt="user"/></a>
+          <a href="#h"><img src="icons/star.svg" alt="fav"/></a>
+          <a href="#h"><img src="icons/shopping-cart.svg" alt="cart"/></a>
           
 
         </div>
+      </div>
         
-        <div className='burguer'>
-          <BurguerButton clicked={clicked} handleClick={handleClick} />
-        </div>
-        <BgDiv className={`initial ${clicked ? ' active' : ''}`}></BgDiv>
-      </NavContainer>
-    </>
+       
+    
   )
 }
 
 export default Navbar
 
-const NavContainer = styled.nav`
-  h2{
-    
-    font-weight: 400;
-    span{
-      font-weight: bold;
-    }
-  }
-  padding: .4rem;
-  
-  display: flex;
-  align-items: center;
-  justify-content: space-between;
-  a{
-    color: black;
-    text-decoration: none;
-    margin-right: 1rem;
-  }
-  .links{
-    position: absolute;
-    top: -700px;
-    left: -2000px;
-    right: 0;
-    margin-left: auto;
-    margin-right: auto;
-    text-align: center;
-    transition: all .5s ease;
-    a{
-      
-      font-size: 2rem;
-      display: block;
-    }
-    @media(min-width: 768px){
-      position: initial;
-      margin: 0;
-      a{
-        font-size: 1rem;
-        
-        display: inline;
-      }
-      display: block;
-    }
-  }
-  .links.active{
-    width: 100%;
-    display: block;
-    position: absolute;
-    margin-left: auto;
-    margin-right: auto;
-    top: 30%;
-    left: 0;
-    right: 0;
-    text-align: center;
-    a{
-      font-size: 2rem;
-      margin-top: 1rem;
-      
-    }
-  }
-  .burguer{
-    @media(min-width: 768px){
-      display: none;
-    }
-  }
-`
-
-const BgDiv = styled.div`
-  
-  position: absolute;
-  top: -1000px;
-  left: -1000px;
-  width: 100%;
-  height: 100%;
-  z-index: -1;
-  transition: all .6s ease ;
-  
-  &.active{
-    border-radius: 0 0 80% 0;
-    top: 0;
-    left: 0;
-    width: 100%;
-    height: 100%;
-  }
-`
