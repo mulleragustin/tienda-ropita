9350b688f78d3382732e8a423b651ed5b6ed9b8f
mulleragustin <agustinmuller98@gmail.com>
2022-12-16 20:05:00 -0300

    solucion error de key

--- src/components/NavList.jsx
+++ src/components/NavList.jsx
@@ -3,10 +3,11 @@ import {  NavLink } from "react-router-dom";
 const NavList = ({categorias}) => {
     return(
         <ul className="navbar-nav mx-auto">
+             
              {categorias.map(categoria => (
-                <li className="nav-item fs-4 ">
-                    <NavLink className="nav-link fs-4 text-dark" activeclassname="page" to={`/category/`+ categoria } key={categoria}>
-                    <p>{categoria}</p>
+                <li className="nav-item fs-4 " key={categoria}>
+                    <NavLink className="nav-link fs-4 text-dark" activeclassname="page" to={`/category/`+ categoria} >
+                    {categoria}
                     </NavLink>
                 </li>)
              )}
